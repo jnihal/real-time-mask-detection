7bdd96b94fc082bc777e29eaf07369b8973210f8
Nihal Jain <nihaljain222@gmail.com>
2021-07-25 01:14:19 +0530

    Add data augmentation and comments

--- mask_detect.py
+++ mask_detect.py
@@ -2,13 +2,12 @@ import os
 import tensorflow as tf
 import numpy as np
 
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.preprocessing.image import load_img, img_to_array, ImageDataGenerator
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.applications import MobileNetV2
 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from tensorflow.python.ops.gen_math_ops import mod
 
 
 TEST_SIZE = 0.2
@@ -18,61 +17,86 @@ DATA_PATH = os.path.join(os.getcwd(), 'data')
 
 def loadData():
 
+    # List to store images and labels
     images = list()
     labels = list()
 
+    # Read the dataset
     for folder in os.listdir(DATA_PATH):
         if folder != '.DS_Store':
             folder_path = os.path.join(DATA_PATH, folder)
             for filename in os.listdir(folder_path):
                 file_path = os.path.join(folder_path, filename)
+
+                # Read and process the image
                 image = load_img(file_path, target_size=(224, 224))
                 image = img_to_array(image)
                 image = preprocess_input(image)
 
+                # Add the image and corresponding label
                 images.append(image)
                 labels.append(folder)
 
+    # Convert labels to numeric form
     le = LabelEncoder()
     le.fit(labels)
     labels = le.transform(labels)
     labels = tf.keras.utils.to_categorical(labels)
 
+    # Convert the list as a numpy array
     images = np.array(images, dtype="float32")
     labels = np.array(labels)
 
+    # Return the training and testing set
     return train_test_split(images, labels, test_size=TEST_SIZE, stratify=labels)
 
 
 def buildModel(save=False):
 
+    # Get the training and testing data
     X_train, X_test, y_train, y_test = loadData()
 
+    # Use the MobileNetV2 model for transfer learning
     base_model = MobileNetV2(
         input_shape=(224, 224, 3),
         weights='imagenet',
         include_top=False
     )
     base_model.trainable = False
+    
 
-    inputs = tf.keras.Input(shape=(224, 224, 3))
-    x = tf.keras.layers.experimental.preprocessing.RandomFlip('horizontal_and_vertical')(inputs)
-    x = tf.keras.layers.experimental.preprocessing.RandomRotation(0.2, fill_mode='nearest')
-    x = tf.keras.layers.experimental.preprocessing.RandomContrast(0.2)
+    # Data augmentation
+    data_augmentation = ImageDataGenerator(
+        rotation_range=50,
+        zoom_range=0.15,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.15,
+        horizontal_flip=True,
+        fill_mode="nearest"
+    )
+
+    # Add custom layers to the base model
     x = tf.keras.layers.AveragePooling2D(pool_size=(7, 7))(base_model.output)
     x = tf.keras.layers.Flatten()(x)
     x = tf.keras.layers.Dense(128, activation='relu')(x)
     x = tf.keras.layers.Dropout(0.5)(x)
-    output = tf.keras.layers.Dense(2, activation='softmax')(x)
+    outputs = tf.keras.layers.Dense(2, activation='softmax')(x)
 
-    model = tf.keras.models.Model(inputs=base_model.input, outputs=output)
+    # Build the model
+    model = tf.keras.models.Model(inputs=base_model.input, outputs=outputs)
 
+    # Compile the model
     optimizer = tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE)
     model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
-    model.fit(X_train, y_train, epochs=20)
 
+    # Fit data to the model with data augmentation
+    model.fit(data_augmentation.flow(X_train, y_train), epochs=20)
+
+    # Evaluate model on the testing set
     print(model.evaluate(X_test, y_test))
 
+    # Save the model
     if save:
         model.save('MaskDetect')
 
--- real_time_detect.py
+++ real_time_detect.py
@@ -1,15 +1,15 @@
-from numpy.core.fromnumeric import shape
 import tensorflow as tf
 import numpy as np
 import cv2
 
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 
 
 def predict(frame, mask_model, face_model):
     height, width = frame.shape[:2]
 
+    # Detect faces using the face_model
     blob = cv2.dnn.blobFromImage(frame, 1, (224, 224), (104, 117, 123))
     face_model.setInput(blob)
     detections = face_model.forward()
@@ -18,9 +18,11 @@ def predict(frame, mask_model, face_model):
     faces = list()
     predictions = list()
 
+    # Loop through the faces
     for i in range(detections.shape[2]):
         confidence = detections[0, 0, i, 2]
 
+        # Get boundary of the face
         if confidence > 0.5:
             box = detections[0, 0, i, 3:7] * np.array([width, height, width, height])
             (x1, y1, x2, y2) = box.astype('int')
@@ -41,6 +43,7 @@ def predict(frame, mask_model, face_model):
 
             faces.append(face)
 
+    # Detect mask if faces are detected
     if len(faces) > 0:
         faces = np.array(faces, dtype='float32')
         predictions = mask_model(faces)
@@ -49,43 +52,59 @@ def predict(frame, mask_model, face_model):
 
 
 def realTimeDetect():
+
+    # Load the models for mask and face detection
     mask_model = tf.keras.models.load_model('MaskDetect')
     face_model = cv2.dnn.readNet('FaceDetect/deploy.prototxt', 'FaceDetect/res10_300x300_ssd_iter_140000.caffemodel')
 
+    # Capture video using webcam
     video = cv2.VideoCapture(1)
 
     while True:
+
+        # Get the frame
         ret, frame = video.read()
 
         if ret == True:
+
+            # Mirror the video
             frame = cv2.flip(frame, 1)
 
+            # Get the boundary and prediction
             coordinates, predictions =  predict(frame, mask_model, face_model)
             
             for coordinate, prediction in zip(coordinates, predictions):
                 (x1, y1, x2, y2) = coordinate
 
+                # Wearing a mask
                 if prediction[0] > prediction[1]:
                     color = (118, 221, 118)
                     label = f"Mask {prediction[0] * 100:.2f}"
+
+                # Not wearing a mask
                 else:
                     color = (98, 105, 255)
                     label = f"No Mask {prediction[1] * 100:.2f}"
 
+                # Draw appropriate labelling
                 cv2.putText(frame, label, (x1, y2 + 25), cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 2)
                 cv2.rectangle(frame, (x1, y1), (x2, y2), color, 4) 
             
+            # Display the frame
             cv2.imshow('Mask Detector', frame)
             cv2.setWindowProperty('Mask Detector', cv2.WND_PROP_TOPMOST, 1)
 
+            # Key to close the webcam
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         
         else:
             break
-        
-
+    
+    # Release the webcam
     video.release()
     cv2.destroyAllWindows()
 
-realTimeDetect()
+
+if __name__ == '__main__':
+    realTimeDetect()
